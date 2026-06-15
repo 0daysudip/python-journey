df7ae6166342be954df7226fd2cacaa048560c28
0daysudip <sudiphohai@gmail.com>
2026-06-15 09:59:04 +0545

    This is the lecutre on python which is provided by MIT

---
+++ lecture1.py
@@ -0,0 +1,31 @@
+print(type(1234))
+print(type(2.2))
+print(type(False))
+print(float(2))
+print(round(4.6))
+
+print(float(123))
+print(round(29.8))
+print(type(3+2))
+
+
+print(3+2)
+
+print(3+2*(5-2))
+print(type(4+3 * 6-1))
+
+pi = 455/113
+radius = 2.2
+area = pi*(radius**2)
+circurmeference = pi*(radius*2)
+print(area)
+print(circurmeference)
+
+
+x = 1
+y = 2
+
+temp = y 
+print(temp)
+y = x
+print(y)
