f5b31ea57ee2de24517039b2656511984b1445b1
0daysudip <sudiphohai@gmail.com>
2026-06-16 19:30:58 +0545

    This is the lecture of second day

---
+++ lecture2.py
@@ -0,0 +1,30 @@
+a = "my"
+b = "myself"
+c = a + b
+d = a + " " + b
+print(c)
+print(d)
+print(a*3)
+
+
+B=":";
+C = ")";
+s1 = B+2*C 
+print(s1)
+
+f = "a";
+g = "b";
+h = "3";
+s2 = (f+g)+h
+print(s2)
+
+s = "abc"
+print(len(s))
+print(s[1])
+print(s[-1])
+
+animal="abcdefghijklmnopqrstuvwxyz"
+print(animal[2:20:2])
+
+verb = input("Type Anything: ")
+print(5*verb)
